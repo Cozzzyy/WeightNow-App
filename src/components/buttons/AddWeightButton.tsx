0a1b8a19b515f9d6c3a036321e89704c8783d7c9
Cosmin Nechita <ioan.nechita@student.kdg.be>
2025-04-02 21:13:51 +0200

    fixed bug

--- src/components/buttons/AddWeightButton.tsx
+++ src/components/buttons/AddWeightButton.tsx
@@ -6,7 +6,7 @@ export function AddWeightButton({ handleOpenDialog }: AddWeightButtonProps) {
     return (
         <button
             onClick={handleOpenDialog}
-            className="text-white font-bold w-[155px] h-[50px] rounded-3xl fixed bottom-4 left-1/2 transform -translate-x-1/2 transition-all duration-200"
+            className="text-white font-bold w-[155px] h-[50px] rounded-3xl mt-4"
             style={{
                 background: 'linear-gradient(93.43deg, rgba(39, 0, 130, 0.66) 17.64%, rgba(67, 0, 223, 0.66) 84.63%)',
                 boxShadow: '-2px 4px 7.3px 6px rgba(0, 0, 0, 0.15)'
