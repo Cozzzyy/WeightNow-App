901d00d258fed28b302eb88db2bfe037de8a76ff
Cosmin Nechita <ioan.nechita@student.kdg.be>
2025-04-06 11:18:44 +0200

    changed pipeline

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -6,11 +6,3 @@ export const createClient = () =>
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     );
-
-export const createAdminClient = () =>
-    createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ADMIN_KEY!,
-    );
-
-
